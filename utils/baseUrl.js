eb6fc8e0b17a2b9feda5912f702182c6cbae8bf3
waltinhocalegari <waltinhocalegari@hotmail.com>
2020-09-11 16:09:28 -0300

    vercel init

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,5 @@
 const baseUrl = process.env.NODE_ENV === "production" 
-? 'convenienciacalegari.com.br' 
+? 'convenienciacalegari-vercel.vercel.app'
 : 'http://localhost:3000';
 
 export default baseUrl;
