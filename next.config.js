e7bb443875d176bdfa2e643819fa21576f41553d
waltinhocalegari <waltinhocalegari@hotmail.com>
2020-09-11 12:41:00 -0300

    conectar o bd

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // must restart server whenever you make changes in next.config
 module.exports = {
   env: {
-    MONGO_SRV: "mongodb+srv://conv-calegari:lorena@cluster0.rz7m5.gcp.mongodb.net/coveniencia?retryWrites=true&w=majority",
+    MONGO_SRV: "kamino.mongodb.umbler.com:41799",
     JWT_SECRET: "hjdfhuiklllllnlkbfcagdahsakjsdjdqw",
     CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/dye38whh3/image/upload",
     STRIPE_SECRET_KEY: "sk_test_51H9DjUDvVpK1u7Jv7qy7QhnjkRr2hvHbfYRUhy8zQUXNZYoYZKI4606QYiVRZaWTvub7vIj35CxgfTQp9e4O9tAJ00sNgbJTac"
