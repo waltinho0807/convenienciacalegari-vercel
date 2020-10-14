634fc8842297b579f48ddd43378b26c4e63409ac
waltinhocalegari <waltinhocalegari@hotmail.com>
2020-10-14 11:26:33 -0300

    alteração banners de cerveja

--- components/Bebidas/CategoryBanner.js
+++ components/Bebidas/CategoryBanner.js
@@ -9,8 +9,8 @@ const CategoryBanner = () => {
                         <img src="https://res.cloudinary.com/dye38whh3/image/upload/v1602676947/bebidas/fd4ujqcc7hpq2fgpswn9.jpg" alt="Categories" />
 
                         <div className="content">
-                            <span>10% Desconto</span>
-                            <h3>Vodkas</h3>
+                            <span>Heineken 600ml</span>
+                            <h3>Temos o melhor preço</h3>
                             <Link href="/products?term=bebidas">
                                 <a className="default-btn">Compre Agora</a>
                             </Link>
@@ -23,8 +23,8 @@ const CategoryBanner = () => {
                         <img src="https://res.cloudinary.com/dye38whh3/image/upload/v1602682414/bebidas/zhsxxdlr81vkeeigcwm6.jpg" alt="Categories" />
 
                         <div className="content">
-                            <span>20% Desconto</span>
-                            <h3>Wiskhy</h3>
+                            <span>Grande variedade</span>
+                            <h3>Em Cervejas de ponta</h3>
                             <Link href="/products?term=bebidas">
                                 <a className="default-btn">Compre Agora</a>
                             </Link>
@@ -37,8 +37,8 @@ const CategoryBanner = () => {
                         <img src="https://res.cloudinary.com/dye38whh3/image/upload/v1602682845/bebidas/limssms92korfurw6cfo.jpg" alt="Categories" />
 
                         <div className="content">
-                            <span>10% Desconto</span>
-                            <h3>Gym</h3>
+                            <span>Budweiser Long. Promoção</span>
+                            <h3>Leve 6 e Pague 5</h3>
                             <Link href="/products?term=bebidas">
                                 <a className="default-btn">Compre Agora</a>
                             </Link>
@@ -51,8 +51,8 @@ const CategoryBanner = () => {
                         <img src="https://res.cloudinary.com/dye38whh3/image/upload/v1602682979/bebidas/rpdai3skyoeyfzwt5ns7.jpg" alt="Categories" />
 
                         <div className="content">
-                            <span>20% OFF</span>
-                            <h3>Artesanais</h3>
+                            <span>Grande Variedade Em</span>
+                            <h3>Cervejas Artesanais</h3>
                             <Link href="/products?term=bebidas">
                                 <a className="default-btn">Compre Agora</a>
                             </Link>
